5b472af3b6cde577c6899aae864e4d71e5b63df8
Dankward Nürenberg <111272245+Dankward15738@users.noreply.github.com>
2025-05-10 07:48:48 +0200

    Add files via upload

---
+++ gpsauslesen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+/home/dankward/Python/gpauslesenein.py
+
+sleep 10
+
+/home/dankward/Python/gpauslesenein.py
+
+sleep 10
+
+/home/dankward/Python/gpauslesenein.py
+
+sleep 10
+
+/home/dankward/Python/gpauslesenein.py
+
+sleep 10
+
+/home/dankward/Python/gpauslesenein.py
+
+sleep 10
+
+/home/dankward/Python/gpauslesenein.py
+
